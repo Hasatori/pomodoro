29066e53aa6e272b6929f7fa14f7803a596aa34e
Hasatori <hradil.o@email.cz>
2019-10-22 20:13:37 +0200

    Removing compilation error part of code.

--- src/app/components/group/group.component.ts
+++ src/app/components/group/group.component.ts
@@ -11,7 +11,7 @@ import {Group} from '../../model/group/group';
 export class GroupComponent implements OnInit {
 
   private groups: Array<Group>;
-@Outpu
+
   constructor(private groupService: GroupService) {
     this.groupService.getGroups().pipe(first()).subscribe(groups => {
       console.log(groups);
