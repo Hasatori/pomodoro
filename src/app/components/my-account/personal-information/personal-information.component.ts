58585eb8ab139baf056929b5eff3d524bb074fa1
Hasatori <hradil.o@email.cz>
2019-12-20 12:07:08 +0100

    Personal information form messages processing changed.

--- src/app/components/my-account/personal-information/personal-information.component.ts
+++ src/app/components/my-account/personal-information/personal-information.component.ts
@@ -1,8 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {UserService} from '../../../services/user.service';
 import {User} from '../../../model/user';
 import {first} from 'rxjs/operators';
 import {nocollapseHack} from '@angular/compiler-cli/src/transformers/nocollapse_hack';
+import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {SuccessComponent} from '../../modals/success/success.component';
 
 @Component({
   selector: 'personal-information',
@@ -11,14 +13,29 @@ import {nocollapseHack} from '@angular/compiler-cli/src/transformers/nocollapse_
 })
 export class PersonalInformationComponent implements OnInit {
   user: User;
-  noValueChanged: boolean = false;
   inProgress: boolean = false;
-  updatedUser: User;
+  submitted: boolean = false;
+  emailError: string;
+  nameError: string;
+  success: string;
+  elegantForm: FormGroup;
+  elegantFormUsername: AbstractControl;
+  elegantFormEmail: AbstractControl;
+  // @ts-ignore
+  @ViewChild('successComponent') successComponent: SuccessComponent;
 
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService, public fb: FormBuilder) {
+    this.elegantForm = fb.group({
+      'elegantFormUsername': ['', [Validators.required]],
+      'elegantFormEmail': ['', [Validators.required, Validators.pattern(/[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}/igm)]],
+    });
+    this.elegantFormUsername = this.elegantForm.controls['elegantFormUsername'];
+    this.elegantFormEmail = this.elegantForm.controls['elegantFormEmail'];
     userService.getUser().pipe(first()).subscribe(
       user => {
         this.user = user;
+        this.elegantFormEmail.setValue(this.user.email);
+        this.elegantFormUsername.setValue(this.user.username);
       }
     );
   }
@@ -26,26 +43,39 @@ export class PersonalInformationComponent implements OnInit {
   ngOnInit() {
   }
 
-  updateUser(firstName: string, secondName: string, username: string, email: string) {
-    this.inProgress = true;
-    this.noValueChanged = false;
-    if (firstName === this.user.firstName && secondName === this.user.lastName && username === this.user.username && email === this.user.email) {
-      this.noValueChanged = true;
-      this.inProgress = false;
-    } else {
-      this.updatedUser = new User();
-      this.updatedUser.firstName = firstName;
-      this.updatedUser.lastName = secondName;
-      this.updatedUser.username = username;
-      this.updatedUser.email = email;
-      console.log(this.updatedUser.firstName);
-      this.userService.updateUser(this.updatedUser).pipe(first()).subscribe(
-        response => {
-          this.inProgress = false;
-            this.user = this.updatedUser;
+  private reset() {
+    this.inProgress = false;
+    this.submitted = false;
+    this.emailError = null;
+    this.nameError = null;
+    this.success = null;
+  }
 
+  updateUser(firstName: string, secondName: string, username: string, email: string) {
+    this.reset();
+    this.submitted = true;
+    console.log(`${firstName} ${secondName} ${username} ${email}`);
+    if (firstName === this.user.firstName && secondName === this.user.lastName && username === this.user.username && email === this.user.email) {
+    } else {
+      this.inProgress = true;
+      let updatedUser = new User();
+      updatedUser.firstName = firstName;
+      updatedUser.lastName = secondName;
+      updatedUser.username = username;
+      updatedUser.email = email;
+      console.log(updatedUser.firstName);
+      this.userService.updateUser(updatedUser).pipe(first()).subscribe(
+        response => {
+          this.reset();
+          this.success = response.success;
+          this.user = updatedUser;
         }
-      );
+        , error1 => {
+          this.reset();
+          this.emailError = error1.error.email;
+          this.nameError = error1.error.username;
+
+        });
     }
   }
 }
