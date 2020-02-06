7a3364d918ea66ea4085a96384c9ebf6eb188f2a
Hasatori <hradil.o@email.cz>
2020-02-06 17:02:57 +0100

    My account layout changed.

--- src/app/components/my-account/my-account.module.ts
+++ src/app/components/my-account/my-account.module.ts
@@ -4,9 +4,20 @@ import {PomodoroHistoryComponent} from './pomodoro-history/pomodoro-history.comp
 import {ChangePasswordComponent} from './change-password/change-password.component';
 import {AuthGuard} from '../../guards/auth-guard.service';
 import {SettingsComponent} from './settings/settings.component';
+import {OverviewComponent} from "../group/overview/overview.component";
 
 export const myAccountRoutes: Routes = [
+  {path:'',component: PersonalInformationComponent, canActivate: [AuthGuard]},
   {path: 'personal-information', component: PersonalInformationComponent, canActivate: [AuthGuard]},
   {path: 'pomodoro-history', component: PomodoroHistoryComponent, canActivate: [AuthGuard]},
   {path: 'change-password', component: ChangePasswordComponent, canActivate: [AuthGuard]},
   {path: 'settings', component: SettingsComponent, canActivate: [AuthGuard]},];
+
+
+/*
+
+{path: '', children: [
+  { path: '',component: OverviewComponent, canActivate: [AuthGuard]},
+  { path: 'overview',redirectTo:'',component: OverviewComponent, canActivate: [AuthGuard]},
+]}
+*/
